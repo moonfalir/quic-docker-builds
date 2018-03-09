426377b10a187fc78262969653a22b096263a45b
jonas reynders <reynders@live.be>
2018-03-09 16:25:00 +0100

    add -u option in bootstrap
    -u allows to override entrypoint script

--- ngtcp2/start-container.sh
+++ ngtcp2/start-container.sh
@@ -1,3 +1,26 @@
-#! /bin/sh
+#!/bin/bash
+
+show_help() {
+   echo "Usage: ${0##*/} [-u] ...
+
+   		-u update startup script"
+}
+#if -u given, override the entrypoint script to first update startup-prep.sh
+override=""
+while getopts uh opt ; do
+	case $opt in
+		u)  override="--entrypoint /quic-docker-builds/ngtcp2/bootstrap.sh"
+			;;
+		h) show_help
+		   exit 0
+		   ;;
+		*) show_help >&2
+		   exit 1
+		   ;;
+    esac
+done
+
 # start container
-docker run --rm -it --net=host --entrypoint /quic-docker-builds/ngtcp2/bootstrap.sh ngtcp2
+dockerrun="docker run --rm -it --net=host"
+dockerrun="$dockerrun $override ngtcp2"
+eval $dockerrun
