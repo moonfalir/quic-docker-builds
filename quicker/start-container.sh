8ca0b417f47ebfe2673b739b6eddb5a3d5b9f668
jonas reynders <reynders@live.be>
2018-03-09 17:29:30 +0100

    Added quicker
    added files to build & run quicker container

---
+++ quicker/start-container.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+show_help() {
+   echo "Usage: ${0##*/} [-u] ...
+
+   		-u update startup script"
+}
+#if -u given, override the entrypoint script to first update startup-prep.sh
+override=""
+while getopts uh opt ; do
+	case $opt in
+		u)  override="--entrypoint /quic-docker-builds/quicker/bootstrap.sh"
+			;;
+		h) show_help
+		   exit 0
+		   ;;
+		*) show_help >&2
+		   exit 1
+		   ;;
+    esac
+done
+
+# start container
+dockerrun="docker run --rm -it --net=host"
+dockerrun="$dockerrun $override quicker"
+eval $dockerrun
---
+++ quicker/startup-prep.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+cd ../ && cd /quicker
+
+# update & rebuild ngtcp2 code
+git pull
+
+npm install
+
+echo "Ready to run"
+
+# open bash
+/bin/bash
