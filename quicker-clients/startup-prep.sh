7c14c211bf549aa0a565d72a5f921b094023616e
jonas <jonas.reynders@student.uhasselt.be>
2018-08-12 22:10:04 +0200

    Checkout correct branches

--- quicker-clients/startup-prep.sh
+++ quicker-clients/startup-prep.sh
@@ -5,10 +5,10 @@ cd ../ && cd /quicker
 # update & rebuild ngtcp2 code
 if [[ $1 == "duplicates" ]];
 then
-	git checkout -b d11-duplicate-packets
+	git checkout -b remotes/origin/d11-duplicate-packets
 elif [[ $1 == "initials" ]]; 
 then
-	git checkout -b d11-sent-2-initial
+	git checkout -b remotes/origin/d11-sent-2-initial
 fi
 
 npm install
