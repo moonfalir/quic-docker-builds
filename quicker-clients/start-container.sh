da39350d04b59842b7ad21eb68151440736c35ab
jonas <jonas.reynders@student.uhasselt.be>
2018-08-12 21:26:26 +0200

    Added container for specific quicker clients

---
+++ quicker-clients/start-container.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+show_help() {
+   echo "Usage: ${0##*/} [-u] ...
+
+   		-u update startup script"
+}
+#if -u given, override the entrypoint script to first update startup-prep.sh
+override=""
+parameter=""
+while getopts uh opt ; do
+	case $opt in
+		u)  override="--entrypoint /quic-docker-builds/quicker-clients/bootstrap.sh"
+			;;
+		h) show_help
+		   exit 0
+		   ;;
+	   	d) parameter="duplicates"
+		   ;;
+	   	i) parameter="initials"
+		   ;;
+		*) show_help >&2
+		   exit 1
+		   ;;
+    esac
+done
+
+# start container
+dockerrun="docker run --rm -it --net=host -v ~/Documents/bachelorproef/quic-docker-builds/quicker-clients/serverlogs:/serverlogs"
+dockerrun="$dockerrun $override quicker $parameter"
+eval $dockerrun
---
+++ quicker-clients/startup-prep.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+cd ../ && cd /quicker
+
+# update & rebuild ngtcp2 code
+if [ $1 = "duplicates"]; then
+	git checkout -b d11-duplicate-packets
+elif [[ $1 = "initials" ]]; then
+	git checkout -b d11-sent-2-initial
+fi
+
+npm install
+
+echo "Ready to run"
+
+# open bash
+/bin/bash
