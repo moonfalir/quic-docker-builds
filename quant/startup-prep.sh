c2cccb95b8afbf69497419d3fa711654be8b755f
jonas <jonas.reynders@student.uhasselt.be>
2018-08-13 17:37:39 +0200

    Copied testfiles to container

--- quant/startup-prep.sh
+++ quant/startup-prep.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-
+cd /quant/Debug/bin/
 echo "Ready to run"
 
 # open bash
--- quicker/startup-prep.sh
+++ quicker/startup-prep.sh
@@ -4,6 +4,7 @@ cd ../ && cd /quicker
 
 # update & rebuild ngtcp2 code
 npm install
+cd ./out/
 
 echo "Ready to run"
 
