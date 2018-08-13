1d67d6fd578e95c56bbb708cb34a12ff6d9afe46
jonas <jonas.reynders@student.uhasselt.be>
2018-08-13 10:06:41 +0200

    use pull instead of checkout: to get latest commit

--- quicker-clients/startup-prep.sh
+++ quicker-clients/startup-prep.sh
@@ -5,10 +5,10 @@ cd ../ && cd /quicker
 # update & rebuild ngtcp2 code
 if [[ $1 == "duplicates" ]];
 then
-	git checkout origin/d11-duplicate-packets
+	git pull origin d11-duplicate-packets
 elif [[ $1 == "initials" ]]; 
 then
-	git checkout origin/d11-sent-2-initial
+	git pull origin d11-sent-2-initial
 else
 	git pull origin d11-baseclient
 fi
